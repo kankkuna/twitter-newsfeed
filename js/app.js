1d99e2f494c8e5d5e471f4e2847675f6541d269f
kankkuna <karina842@hotmail.com>
2017-11-19 20:03:51 -0500

    mejora de ingreso de tweet

--- js/app.js
+++ js/app.js
@@ -1,21 +1,26 @@
 window.addEventListener('load', function() {
-  var addText = document.getElementById('add');
-  var button = document.getElementById('saveText');
-  var text = document.getElementById('writeInput');
+  var addText = document.getElementById('tweet');
+  var button = document.getElementById('press');
+  var text = document.getElementById('insert');
+
 
   button.addEventListener('click', function(event) {
     event.preventDefault();
-     // obteniendo el contenido del input
-    var save = document.createElement('div');
-    save.setAttribute('class','save-div')
-    var paragraph = document.createElement('p');
-    paragraph.setAttribute('class','new-paragraph');
-    paragraph.textContent = text.value;
-    save.appendChild(paragraph);
-    addText.appendChild(save);
+    if (text.value) {
+      var save = document.createElement('div');
+      save.setAttribute('class', 'post');
+      var paragraph = document.createElement('p');
+      paragraph.setAttribute('class', 'new-paragraph');
+      paragraph.textContent = text.value;
+      save.appendChild(paragraph);
+      addText.appendChild(save);
 
-    text.value = '';
+      text.value = '';
 
-    addText.insertBefore(save , addText.childNodes[0]);
+      addText.insertBefore(save, addText.childNodes[0]);
+    }
   });
 }) ;
+
+function(){
+}
